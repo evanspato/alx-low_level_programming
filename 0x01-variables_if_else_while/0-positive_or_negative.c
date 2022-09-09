071c63e69044d801fc2b5c3a74f2f7ce7086b72e
evanspato <evanspato3@gmail.com>
2022-09-09 15:30:05 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,14 +12,13 @@
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if(n > 0)
 	{
 		printf("%i is positive\n", n);
 
+	
 	}
 	else if(n == 0)
 	{
@@ -29,10 +28,11 @@ int main(void)
 	else
 	{
 		printf("%i is negative\n", n);
-
-		return (0);
 	
 	}
-	
-       }
+
+	return (0);
+
+
+}
 
