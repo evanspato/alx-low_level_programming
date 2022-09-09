186d1c7cad39ec9c5d2f54706e4d5dfde2910bc0
evanspato <evanspato3@gmail.com>
2022-09-09 16:30:00 -0700

    0-positive_or_negative.c

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -18,7 +18,7 @@ int main(void)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if(n == 0)
+	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
@@ -28,8 +28,6 @@ int main(void)
 	}
 
 	return (0);
-
-
 }	
 	
 	
