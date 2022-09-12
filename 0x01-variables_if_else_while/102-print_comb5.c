4e9f5f5cd8026560d54322103ef4fab11b78ebb1
evanspato <evanspato3@gmail.com>
2022-09-12 09:47:23 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0 i < 100; i++)
+	for (i = 0; i < 100; i++)
 	{
 		for (j = 0; j < 100; j++)
 		{
@@ -19,13 +19,13 @@ int main(void)
 				putchar(' ');
 				putchar((j / 10) + 48);
 				putchar((j % 10) + 48);
-				if (i != 98 || j != 99)
+				if (1 != 98 || j != 99)
 				{
 					putchar(',');
 					putchar(' ');
 				}
-
-			}	}
+			}
+		}
 	}
 	putchar('\n');
 	return (0);
