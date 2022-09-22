3ace1a11d024b5eadc7ca53cc73ffe72b7ce6345
evans <evanspato3@gmail.com>
2022-09-22 06:41:30 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,21 +3,13 @@
 
 
 /**
-
- *_strcat - concatenates  the string pointed to by @src to
-
+ * _strcat - concatenates  the string pointed to by @src to
  * the end of the string pointed to by @dest
-
- *@dest: String that will be appended
-
- *@src: String to be concatenated upon
-
+ * @dest: String that will be appended
+ * @src: String to be concatenated upon
  *
-
  * Return: returns poiner to @dest
-
  */
-
 
 
 char *_strcat(char *dest, char *src)
