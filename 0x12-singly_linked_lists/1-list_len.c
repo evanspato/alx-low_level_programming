f37c4da4f4d52a8689b33d899aa2a3cd03be0889
evanspato <evanspato3@gmail.com>
2022-10-20 07:20:26 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,13 +5,11 @@
 
 
 /**
- * print_list - Prints all the elements of a list_t list. 
- * @h: The list_t list. 
+ * print_list - Prints all the elements of a list_t list.
+ * @h: The list_t list.
  *
- * Return: The number of nodes in h. 
+ * Return: The number of nodes in h.
  */
-
-
 
 size_t print_list(const list_t *h)
 
