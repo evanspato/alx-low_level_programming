62e22bee2ddeb40c8cdda05b2358fdb2ec5e91a5
evanspato <evanspato3@gmail.com>
2022-10-26 01:48:56 -0700

    lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -15,19 +15,11 @@
 
 
 /**
-
  * struct listint_s - singly linked list
-
  * @n: integer
-
  * @next: points to the next node
-
  *
-
  * Description: singly linked list node structure
-
- *
-
  */
 
 
