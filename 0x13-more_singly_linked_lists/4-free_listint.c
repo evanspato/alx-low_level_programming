bb60f7f4508f4911525d27c89eb267b3b8258e2e
evanspato <evanspato3@gmail.com>
2022-10-26 02:33:18 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,18 +1,11 @@
 #include "lists.h"
 
 
-
 /**
-
  * free_listint - frees a listint_t list
-
  * @head: points to lists_t list
-
  * Returns: nothing
-
  */
-
-
 
 void free_listint(listint_t *head)
 
